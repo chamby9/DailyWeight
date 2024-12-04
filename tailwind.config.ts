3d31f8391f6762004038dffb464eb13aebf30763
chamby9 <52965054+chamby9@users.noreply.github.com>
2024-12-04 14:04:47 +0000

    feat(theme): configure daisyui themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,51 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      {
+        weightLight: {
+          "primary": "#3B82F6",
+          "secondary": "#10B981",
+          "accent": "#F97316",
+          "neutral": "#374151",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#22C55E",
+          "warning": "#FBBF24",
+          "error": "#EF4444",
+        },
+        weightDark: {
+          "primary": "#3B82F6",
+          "secondary": "#10B981",
+          "accent": "#F97316",
+          "neutral": "#1F2937",
+          "base-100": "#111827",
+          "info": "#3ABFF8",
+          "success": "#22C55E",
+          "warning": "#FBBF24",
+          "error": "#EF4444",
+        },
+      },
+    ],
+    darkTheme: "weightDark",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: false,
+  },
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+};
+
+export default config;
